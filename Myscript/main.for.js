050ab50b0c426a3aa1f5e50220fdc8277cfb83f1
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-04-25 15:49:18 +0200

    creo percorsi e aggiungo immagini alla pagina

---
+++ Myscript/main.for.js
@@ -0,0 +1,7 @@
+// trovare tutti gli elementi con class .slide
+
+//iterare su tuttte le slide (creare un ciclo)
+
+  //per ogni slide ccontrolliam o se ha la classe .active 
+  // se si,rimuovo la clasee4 active e prendo nota dell indice
+
