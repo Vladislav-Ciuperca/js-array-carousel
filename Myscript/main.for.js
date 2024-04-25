0650b6278d3ba1d20a17c9278783b27d3d0bec31
Vladislav-Ciuperca <ciuperca37@gmail.com>
2024-04-25 16:07:51 +0200

    creo il readme

--- Myscript/main.for.js
+++ Myscript/main.for.js
@@ -1,7 +1,15 @@
 // trovare tutti gli elementi con class .slide
+let slides = document.getElementsByClassName("slide")
 
 //iterare su tuttte le slide (creare un ciclo)
+for (let i = 0; i < array.length; i++) {
+    const slide = slides[i];
+    console.log(slide, i)
 
-  //per ogni slide ccontrolliam o se ha la classe .active 
-  // se si,rimuovo la clasee4 active e prendo nota dell indice
+}
+
+//per ogni slide ccontrolliam o se ha la classe .active
+// se si,rimuovo la clasee4 active e prendo nota dell indice
+
+//per ogni slide controllo che sia quella successiva a chi e attualmente active
 
---
+++ readme.md
@@ -0,0 +1,6 @@
+inserisco delle immagini e do a tutte la classe slide cke oltre alla specifica grandezza avra anche "display-none"
+doppo di che creo una classe "active" che avra "display-block"
+faccio creare a JSn un array usando "document.getelement..."
+creo un ciclo for che vada a leggere questo array e controllo quale elemento ha la classe "active"
+quado trolo l'elemento con questa classe,rimuovo la suddetta classe e prendo nota dell indice.
+
